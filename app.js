90fb66b928104b0b6638951f2354ee9bba1dcb7f
[Tomas Valdez] <Tomasvaldez02@gmail.com>
2023-07-05 14:33:01 -0300

    se agregaron dependencias necesarias, middlewares, estableci un motor de plantillas ejs y modifique las rutas

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
 // Imports
-const cors = require('cors');
-const express = require('express');
-
-const path = require('path');
+const cors = require("cors");
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const dotenv = require("dotenv");
+const path = require("path");
+require("dotenv").config();
+require("ejs");
 
 const app = express();
+const port = process.env.PORT || 4000;
+
+const { sequelize } = require("./database");
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database:", err);
+  });
+
+require("ejs");
 
 // Middlewares
 // TODO: Implementar middlewares
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("view engine", "ejs");
 
 // Routes
-app.use('/api', require('./routes/reserva.routes'));
+app.use("/", require("./routes/reserva.routes"));
 
 // TODO: Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
+app.use((req, res, next) => {
+  res.status(404).send("Error 404");
+});
 
 // Starting the server
-app.listen(45635, () => console.log('Server on port xxxx'));
+app.listen(process.env.PORT, () => console.log("Server on port: " + port));
